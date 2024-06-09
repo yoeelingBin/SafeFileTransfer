8bdbb09757176eba78aba058cc696ac579b888db
Yan Qibin <57822313+yoeelingBin@users.noreply.github.com>
2024-06-09 23:48:31 +0800

    add cs handling funcs

--- client/client.py
+++ client/client.py
@@ -23,32 +23,70 @@ OQIDAQAB
 -----END PUBLIC KEY-----
 '''
 CLIENT_PRIVATE_KEY = '''-----BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEAhm9dbOXVREZQnIW4k6kPkNeUkVm/13VX1npIch/z5YXb04OK
-nTTjQYZznbsJZLk3kUqz5UV/i5zGxmlI45K+thK6qN59SpN7EXxRQ3YULNQGIxX6
-3xY3TVfUKVRzyEtODL0foP1fF8U7X2pDTuGaNyckJnOLSlj3KEiRnKyARBr6qlfS
-OPpA88jT0leQmHKtaa785/oD1Oe8oAHSCgZGK1tGT+AhERnoagpEkbSGx06UVnnX
-++tipzBcLiPRzOXzQSNdzvylPXuYs8TBnZ8SaHDId64f8rxW71lEaeD64p7iDQOj
-EV6excQcjcTu2fZZ+fuTV6TsrjzzsBy+5ztxEQIDAQABAoIBABJ2mL1cOB+h4UVg
-pNp8JXFOTQ8aD/EpTRCIXTXLXKXBGNFiZ2Gy7KPzBs2lbU4CQCGkwbNIib1ky/Sz
-wLknTikBxoAV2ohTsVspQ0dBdYDjTRp5f38vH8DwFz6ROjuf2A7Y1KS5gltokQBy
-6YlGY8s/sBqAfk4ON5e2lm+5o1N3BtDFob1GEBZn4ekxoiol/FCfD1V7n7cazLIy
-SqROelB2zC4OQZVyysH/OYCu7bHl5xZLkycpvMMuzTAZV2t6+PtjzksYurYjrEHH
-gvRTCmUxTZ2vJ4uN2OUpsA/8JGWT+omLYzwP+gwOLItFZM8lbkRn+hk0ytd1EZTd
-gFFcNt8CgYEAt6gSUXCwkujwKso8Zom1nSucI8HsYTmvot76c6NPkeXpgz628DtA
-dbuhx+qGyPf9hExqCcggsYMf9Z9Itp5cWeHT9SHY8EZ4H3UoBbJqBO1K7wH2VXfW
-wb4f7cFdj/Alb/j4eCX06Xh+f8Xh4Y+zK91q2gdiwsDf26vCo2S7SXcCgYEAu2PL
-k9J2HVmsdRqunYrSNgKZwi0JdA4HYXXu6wsOehcoKinNhNKsFQh73rpOyOlsZ7cM
-ucsU5N6LtEwMyyH0fnX17P2LOLmunZ0QiahDzFjvqeyonFdtnd3jGezi6h0cug3r
-2ZsZ+STlsIkDaWNl7dLReLZTMvW5LYVY80rQu7cCgYAlz9e9prrj9EuGM88NuWqH
-6nOiNQnq1oqfuNLNviDiw/g4yk+11C0oUDI1y8ZmWBB9DZKDYw3AHmQVGO9Kkf4n
-j0QpYwecKJYQu/k6ewoy63wTC09hN2QizXcbZ0VRHjtG+3aEniP3cZRx0Bb6brCP
-5m1aaEzgFf/A5XMUWufobwKBgQC7Na4y1nZOSEJ1E90TY5czglGenmPtX/6brbit
-dXRXIT9tMCo04kwtDzbBg5wmOJ2m2EeMbolHRdaIn9nALwIWPT8eLweh+k+rAzl8
-bXCefNOjDd49o1LN/tleEz136vHCJxpTYhMGx4f5YEjaRwWUHaRMVsq0BK2l/qyp
-EzuJ7QKBgEhVwgSqUA213Ei8hUb9XEo6JKK/l5qyv+2RcY3HpTLdS2eZd0VwtQmt
-OdWeNtIDT9gar0AUhg7eEx6hu/ZHjpNYYHgQ/CmSgEegpAc9Jnkp2/oHtZ0ETxlD
-g1U5DpBKWxwGNHgxnctv9Y9oGJHOJG7HQLPbThIkLoA2Z+JfDNGH
+MIIEowIBAAKCAQEA3KVnjnA2BXxf6leV3EjRSdAHTdEHwmjjiJgFFBJiB+V4RCAE
+FDWiRA8tiU+LkAPZwH/XE8dWkO/cCc3f6LHrMC/BosVPLvxMWrkkQkH9eS/eTja9
+RpF0RxJozVT7bRJDhKd/dkOKdiqGzuzoR0YIJ+agFImPdauS/tlLK5ja+/RU6yL4
+iDs6u191h7vB4wAa2N2c/KSFOYjjbybskN77jyyFEA2miixqJJRAn6bRsa6PKRZ3
+X1jkcxUZqigRvxtEGhhYmZroKSJ/9OzuKz6vY9ZtqOR7NNxdYiMbGD/TljRxF+y/
+aSpavbPzMpRSLPz5yF4BCVm5r/4YZtII8wKmKwIDAQABAoIBAAf6PPvD59gb7pXY
+89Sil1qNWG5CU/797tgV8v0OSxgQ/l+sLqmSZNsEF3fi9d9PnFRe6uZOqz0TRwfd
+ty5BHIlB+MTLUHkY6yPOlNaGb0Ut62I1jXNbN/KH4l0mKO8BHYrSyYN7nqp2ECi0
+HRPSnuHeb0Q+0v6EpQxsy9B4NtasJiNan2snCEQ7i6OAGNsuIvc79MmIWmdSiRm7
+lC3ZpSNyom25Qo4DQbzbZ/oeozJ4N4CppRG0Lmhcvv6/2G43lWaGa7EuSoOUdOgR
+Ba7RPlPnGpW147yHleSGet1qmASf818qIOypNHoay1BAZgWmdszQv2/VhLTnZ9dv
+oSSviHUCgYEA6PoNAj2VuL/LmZQhU5NxiK99ELzALyFQQvsMaG4eafud5X2P6U/G
+lDe9kZNjLBacpeesiPf39S5r3V4YL3nf6emcXcTUIwZbG1fGKxge9Ixh4HgC+1ve
+B7NS702SXZmQ6VDCZi0obLKmJweaH7enT5GaLo8PVVsQ5R/B4SmSBI8CgYEA8nNo
+UBjKoMynlyAXEfX+KP+8fL9Ba1wFzWxOKyw5hTIhPxuuYYbpBdQU/JBGvu3yGLKt
+blbST2s28emMDlgHewWOzywTBmffmGqYt4gjjLFg8ZRxMql5q53PiOEL1zLunkq0
+JL/2yXA0KRO51HVj4KK+0zmiOUlM18j747AV6qUCgYBT92sg15lSkK2MmHq6aHWO
+0dC9a4nIcrU+rsR2DtofUHRD9dEcQYhMexpzkS85AJ1MngbtBpHzZ9uwWO1WjxJI
+d83Hbd0XEn9bh3MArRza/o14HUjV1vJazCKj5M1Ptr0nmde2g6gCJREFGBRQQhym
+7M8o4J6iIMQiECQMRrM9uQKBgFGjsthlwLVstHIbCCmwH6lGk/2dmTXBguKtOZUo
+CyZivvc1Jv8IIqcnxvlUy722+fJ/GA8zhRXhEFtdPSAHXF18fZ4dRTq+93enTU1f
+tjjF8dLnHUbl8mZreVqqDQaly9vZY9eMHFmwQqAiWEzGSp91rbQKkCmiGRIAR3Ff
+9cFRAoGBAMIKBnWWMOHSxvxS3XvNCQ+NiYylFGdFS0/VHQiDqvt8zQ3bkHNQ5Hpk
+XaY6GW2VVq/cDALzQOw2cb0uvyEFmFe78mYdsQAR4hGmA47QASXT6lqeYUuvnMGJ
+V5eKqdttensnCsFam8W6wVklBsO2KiS2xpqY9OfI1efUYz14nYIh
 -----END RSA PRIVATE KEY-----
+'''
+SERVER_PRIVATE_KEY = '''-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAv8TQORU+5RHbuXv40mtFCYCmG3ML2a4VkhtLEDRArO0M+Dpb
+Blikbw5hPVHwcfSyqayFRFA+VUzSa8jwTwULz2MgCSav57nouwpGqJA8hznezTpl
+XMU1Gbjic8N1MDwIaRAtnQrgS+bRrrp/1ry+ygNcJRlt8e0hfJR1arHVqSKW897G
+cnGYXwVKZ7AwUHW54PzPegfc8lMGVWPHctdriYcMCynUK8LDGraayg06vy6OwsLI
+15m2W+q2eUbrdcHFYV3hmeWNhhvcgMPdYnfWaG//ImhD/rO4zLNZjtrKzsYWeGpv
+rmwgg5czU7X3rl2fm9Shhm9iB8c3QmF+tuXMOQIDAQABAoIBAAZbwz+xSaWUwwFS
+b/yiG+blwhw041azY/m1N/bwlJhnP7/XR39dXw5jnqvG1L8iiu3/T2fldTuk0XFL
+d1RXaX3V3pEPHFQFoQban18lcSxWqeGKJyQ9UbZpn/CQsYkI5Ip/Q9PqMWey3o75
+lhWLqpPOKrb8Md4Mq8iSr5X7EYeLFEfcLrxlJvJuAnqjMb8zK9h+/LGa8uUDSlxC
+cvSTm26g9IxSMpKC/CKXW8Dhi+y6J/F677G9yu6n//OqK3UXIyPYS9uALNrXSmKq
+GzbAcOxkIetFI/fm7L2vFk+vtdoLKgEw1OQ9+UhP3MqdOCkoFNAzcUNQNswxGyB2
+UVi3k6UCgYEAxm1dFOUvekxW2Ld5Km/oKNgmnzfIhcHzWsu4q0hxPPmYjM5m/Wcw
+v432QGzAaC7Y/VuvfH8dRFSvh6264O27jLia2hRv8uXxGL93nfthN03frI077Z4T
+DNY20jhekPfnRaHX/f3/L1sEFBDSJPEotqMk9gbnSvyB88GAFjIzXZ8CgYEA92jh
+wYF9kiohb9hPAsSGI6Dyyzns0KexYsjWM/XKM3uBREzQKfnBLeSyaS4kFVRcFhlV
+1wud8Rd/NH1OpeQy9jd+2k5wQrzlOvvKjcNBitw8Z9vaFAuzF5GpaiXT0BNOBbFd
+EROjEpUtjuN2hcPkr3J4mSYO2dpkF5yDazlO1ycCgYAyR/5BUD4ysGuFaSC0Bz1+
+NB+9UuZmNpqTFHKMPMQtHlZwv9DLP73TnIadFrG+9LgZo4UZeCCTcx4ztGtZmgRf
+iVv8DRe9JlVs3v/RsaSV3g7i67sW4GqVYybrKEuWUqtxMqzH+PgUKO0kpIqLv+yA
+M2EnUuKDVu8bNJpfhYMMowKBgFf8kcTqk0jG+Os4xyiZ0YacN5x0tjaiXKBgAmWq
+NMXIV3l9w84Zx0zV92kWgrifLryhN/jpZbsW+yMkqTKwDDuqEs/6c0wYt4EZiLiP
+xyBmIIljE5RrcL6iC9j3KPPn2aiGoi1viWATc4dMd5ssxohqsl7svP1XAJ+xBr+8
+OsCjAoGAC70z9Js1wiBlAQX5pq59rlsU+76KUSf8QtPxC3KTtsbRXwJjVyzvRMHg
+Z9uOhaAKGAVskyes+6yCrajr6OYDYniJnoUKX5AauxT1J0pgasAN4mZ3MrDkEy4r
+msT6Tt/GSW50sLrjf1v3M26FJS9dq7v0Tbl34Ka03CzaGD6L4Ho=
+-----END RSA PRIVATE KEY-----
+'''
+CLIENT_PUBLIC_KEY = '''-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3KVnjnA2BXxf6leV3EjR
+SdAHTdEHwmjjiJgFFBJiB+V4RCAEFDWiRA8tiU+LkAPZwH/XE8dWkO/cCc3f6LHr
+MC/BosVPLvxMWrkkQkH9eS/eTja9RpF0RxJozVT7bRJDhKd/dkOKdiqGzuzoR0YI
+J+agFImPdauS/tlLK5ja+/RU6yL4iDs6u191h7vB4wAa2N2c/KSFOYjjbybskN77
+jyyFEA2miixqJJRAn6bRsa6PKRZ3X1jkcxUZqigRvxtEGhhYmZroKSJ/9OzuKz6v
+Y9ZtqOR7NNxdYiMbGD/TljRxF+y/aSpavbPzMpRSLPz5yF4BCVm5r/4YZtII8wKm
+KwIDAQAB
+-----END PUBLIC KEY-----
 '''
 
 class client:
@@ -61,15 +99,15 @@ class client:
         # 将socket打包成SSL socket
         self.ssock = context.wrap_socket(self.sock, server_hostname=SERVER_ADDRESS)
     
-    def upload_file(self, filepath: str):
-        if os.path.isfile(filepath):
+    def upload_file(self, file_path: str):
+        if os.path.isfile(file_path):
             # 定义打包规则
             fileinfo_size = struct.calcsize('128sl')
             # 定义文件头信息，包含文件名和文件大小
             header = {
                 'command': 'UPLOAD',
-                'fileName': os.path.basename(filepath),
-                'fileSize': os.stat(filepath).st_size,
+                'fileName': os.path.basename(file_path),
+                'fileSize': os.stat(file_path).st_size,
                 'time': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
             }
             header_bytes = bytes(json.dumps(header).encode("utf-8"))
@@ -78,11 +116,11 @@ class client:
             print(fhead)
             self.ssock.send(fhead)
             # 将传输文件以二进制的形式分多次上传至服务器
-            fp = open(filepath, 'rb')
+            fp = open(file_path, 'rb')
             while True:
                 data = fp.read(1024)
                 if not data:
-                    print('{}文件发送完毕...'.format(os.path.basename(filepath)))
+                    print('{}文件发送完毕...'.format(os.path.basename(file_path)))
                     break
                 print("发送的内容:", data)
                 tosend = encrypt_file(data)
@@ -92,6 +130,10 @@ class client:
 
             # fp.close()
             self.ssock.close()
+    
+    def download_file(self, file_path: str):
+        #TODO 
+        pass
 
 
 def encrypt_file(data) -> bytes:
@@ -104,8 +146,9 @@ def encrypt_file(data) -> bytes:
     print("aes加密密钥:", aes_key, "aes初始向量:", aes_iv)
     # 消息摘要
     digest = rsa_cipher.sign_message(data, CLIENT_PRIVATE_KEY)
+    print("消息摘要:", digest)
     # 将摘要和消息打包
-    concated_message = {"Message": base64.b64encode(data), "Digest": digest.decode("utf-8")}
+    concated_message = {"Message": base64.b64encode(data), "Digest": digest}
     dumpped_message = pickle.dumps(concated_message)
     # 对称加密消息
     cipher_message = aes_cipher.encrypt_message(dumpped_message)
@@ -120,10 +163,29 @@ def encrypt_file(data) -> bytes:
     # 发送消息
     print("序列化前的发送消息", [cipher_message, cipher_keyiv])
     send_message = pickle.dumps([cipher_message, cipher_keyiv])
-    print("发送的消息:", send_message)
+    # print("发送的消息:", send_message)
     return send_message
 
-
+def decrypt_file(data):
+    rsa = RSACryptor()
+    cipher_message, cipher_keyiv = pickle.loads(data)
+    print("密文:{}(类型{}) \n密钥:{}(类型{})".format(cipher_message, type(cipher_message), cipher_keyiv, type(cipher_keyiv)))
+    decrypted_keyiv = rsa.decrypt_message(cipher_keyiv, SERVER_PRIVATE_KEY)
+    # print("接收到的密钥和初始向量:", decrypted_keyiv)
+    keyiv = pickle.loads(decrypted_keyiv)
+    key, iv = keyiv["Key"], keyiv["IV"]
+    print("解密后的密钥{}和初始向量{}:".format(key, iv))
+    aes = AESCryptor(key, iv)
+    decrypted_message = aes.decrypt_message(cipher_message)
+    plain_message = pickle.loads(decrypted_message)
+    content = base64.b64decode(plain_message['Message'])
+    print("解密的内容是", content)
+    digest = plain_message['Digest']
+    print("解密的消息摘要", digest, type(digest))
+    if rsa.verify_signature(content, digest, CLIENT_PUBLIC_KEY):
+        return content
+    else:
+        print("文件签名不一致!")
 
 # def upload_file(file_path):
 #     nonce, ciphertext, tag = encrypt_file(file_path)
@@ -146,45 +208,16 @@ def encrypt_file(data) -> bytes:
 
 #     print("File send success")
 
-def download_file(file_name, save_path):
-    context = ssl.create_default_context()
-    context.load_verify_locations("certs/server.crt")
-    with socket.create_connection((SERVER_ADDRESS, SERVER_PORT)) as sock:
-        with context.wrap_socket(sock, server_hostname=SERVER_ADDRESS) as ssock:
-            header = b'DOWNLOAD' + f'{len(file_name):016d}'.encode()
-            ssock.sendall(header)
-            ssock.sendall(file_name.encode())
-
-            response = ssock.recv(4096)
-            if response.startswith(b'File not found'):
-                print("File not found on server")
-                return
-            
-            data_length = int(response[:16].decode())
-            data = response[16:]
-            while len(data) < data_length:
-                data += ssock.recv(4096)
-                
-            nonce, ciphertext, tag, file_hash = data[:16], data[16:-48], data[-48:-32], data[-32:].decode()
-            decrypted_data = decrypt_file(nonce, ciphertext, tag)
-
-            with open(save_path, 'wb') as f:
-                f.write(decrypted_data)
-            
-            if sha256_hash(save_path) != file_hash:
-                print("File integrity check failed")
-            else:
-                print("File downloaded successfully")
-
-# Example usage:
-# upload_file('path_to_your_file')
-# download_file('file_name_on_server', 'path_to_save_file')
 if __name__ == "__main__":
     mes = b"kskskkqqqhfhfh1234"
     file_path = "1.txt"
-    # client = client()
-    # client.upload_file(file_path)
-    fp = open(file_path, 'rb')
-    data = fp.read(1024)
-    encrypt_file(data)
-    fp.close()
+    file2_path = "2.md"
+    file3_path = "3.jpg"
+    client = client()
+    client.upload_file(file3_path)
+    # fp = open(file_path, 'rb')
+    # data = fp.read(1024)
+    # enc_data = encrypt_file(data)
+    # dec_data = decrypt_file(enc_data)
+    # print(dec_data)
+    # fp.close()
--- server/server.py
+++ server/server.py
@@ -15,14 +15,42 @@ SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 12345
 UPLOAD_DIR = 'uploaded_files'
 CLIENT_PUBLIC_KEY = '''-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqxKcgrLBuvIzR575zEd+
-11ZewWBF/lO+B5GdecNlRGbJ7HOoWDWe9o7dV5pqmArliALFN/7RvwYMTtaUMidJ
-YGsDG9OJxq8/lLcKsx34fdzZEel1bb60RyMfNXGpCA7P69auK8ljeaEU2WxoMtkZ
-bz2RcP/+zwDj05a6edhXqAdZjqx2Pu0a8SSdS19eeIbkRzEQ9Gm8MDuJVfLU9QAs
-LzWI7YrpRnw9XqaRzZas0MCfIsakm88pOcWuQ4qpl0X2/AjPrQmiyPbNLnw/3baL
-r8kyLApo6Ivt6gCVpkmRREH4aecU4TY3Ce5rPJQcJbxrp4M0EvyS0Q7hvKW9SY3s
-0wIDAQAB
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3KVnjnA2BXxf6leV3EjR
+SdAHTdEHwmjjiJgFFBJiB+V4RCAEFDWiRA8tiU+LkAPZwH/XE8dWkO/cCc3f6LHr
+MC/BosVPLvxMWrkkQkH9eS/eTja9RpF0RxJozVT7bRJDhKd/dkOKdiqGzuzoR0YI
+J+agFImPdauS/tlLK5ja+/RU6yL4iDs6u191h7vB4wAa2N2c/KSFOYjjbybskN77
+jyyFEA2miixqJJRAn6bRsa6PKRZ3X1jkcxUZqigRvxtEGhhYmZroKSJ/9OzuKz6v
+Y9ZtqOR7NNxdYiMbGD/TljRxF+y/aSpavbPzMpRSLPz5yF4BCVm5r/4YZtII8wKm
+KwIDAQAB
 -----END PUBLIC KEY-----
+'''
+SERVER_PRIVATE_KEY = '''-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAv8TQORU+5RHbuXv40mtFCYCmG3ML2a4VkhtLEDRArO0M+Dpb
+Blikbw5hPVHwcfSyqayFRFA+VUzSa8jwTwULz2MgCSav57nouwpGqJA8hznezTpl
+XMU1Gbjic8N1MDwIaRAtnQrgS+bRrrp/1ry+ygNcJRlt8e0hfJR1arHVqSKW897G
+cnGYXwVKZ7AwUHW54PzPegfc8lMGVWPHctdriYcMCynUK8LDGraayg06vy6OwsLI
+15m2W+q2eUbrdcHFYV3hmeWNhhvcgMPdYnfWaG//ImhD/rO4zLNZjtrKzsYWeGpv
+rmwgg5czU7X3rl2fm9Shhm9iB8c3QmF+tuXMOQIDAQABAoIBAAZbwz+xSaWUwwFS
+b/yiG+blwhw041azY/m1N/bwlJhnP7/XR39dXw5jnqvG1L8iiu3/T2fldTuk0XFL
+d1RXaX3V3pEPHFQFoQban18lcSxWqeGKJyQ9UbZpn/CQsYkI5Ip/Q9PqMWey3o75
+lhWLqpPOKrb8Md4Mq8iSr5X7EYeLFEfcLrxlJvJuAnqjMb8zK9h+/LGa8uUDSlxC
+cvSTm26g9IxSMpKC/CKXW8Dhi+y6J/F677G9yu6n//OqK3UXIyPYS9uALNrXSmKq
+GzbAcOxkIetFI/fm7L2vFk+vtdoLKgEw1OQ9+UhP3MqdOCkoFNAzcUNQNswxGyB2
+UVi3k6UCgYEAxm1dFOUvekxW2Ld5Km/oKNgmnzfIhcHzWsu4q0hxPPmYjM5m/Wcw
+v432QGzAaC7Y/VuvfH8dRFSvh6264O27jLia2hRv8uXxGL93nfthN03frI077Z4T
+DNY20jhekPfnRaHX/f3/L1sEFBDSJPEotqMk9gbnSvyB88GAFjIzXZ8CgYEA92jh
+wYF9kiohb9hPAsSGI6Dyyzns0KexYsjWM/XKM3uBREzQKfnBLeSyaS4kFVRcFhlV
+1wud8Rd/NH1OpeQy9jd+2k5wQrzlOvvKjcNBitw8Z9vaFAuzF5GpaiXT0BNOBbFd
+EROjEpUtjuN2hcPkr3J4mSYO2dpkF5yDazlO1ycCgYAyR/5BUD4ysGuFaSC0Bz1+
+NB+9UuZmNpqTFHKMPMQtHlZwv9DLP73TnIadFrG+9LgZo4UZeCCTcx4ztGtZmgRf
+iVv8DRe9JlVs3v/RsaSV3g7i67sW4GqVYybrKEuWUqtxMqzH+PgUKO0kpIqLv+yA
+M2EnUuKDVu8bNJpfhYMMowKBgFf8kcTqk0jG+Os4xyiZ0YacN5x0tjaiXKBgAmWq
+NMXIV3l9w84Zx0zV92kWgrifLryhN/jpZbsW+yMkqTKwDDuqEs/6c0wYt4EZiLiP
+xyBmIIljE5RrcL6iC9j3KPPn2aiGoi1viWATc4dMd5ssxohqsl7svP1XAJ+xBr+8
+OsCjAoGAC70z9Js1wiBlAQX5pq59rlsU+76KUSf8QtPxC3KTtsbRXwJjVyzvRMHg
+Z9uOhaAKGAVskyes+6yCrajr6OYDYniJnoUKX5AauxT1J0pgasAN4mZ3MrDkEy4r
+msT6Tt/GSW50sLrjf1v3M26FJS9dq7v0Tbl34Ka03CzaGD6L4Ho=
+-----END RSA PRIVATE KEY-----
 '''
 
 def init_key():
@@ -30,67 +58,6 @@ def init_key():
     rsa = RSACryptor()
     rsa.gen_rsa_key_pairs()
     SERVER_PUBLIC_KEY, SERVER_PRIVATE_KEY = rsa.public_key, rsa.private_key
-
-def handle_client(connection):
-    header = connection.recv(22)  # 6 bytes for command and 16 bytes for filename length
-    command = header[:6]
-    filename_len = int(header[6:22].decode())
-    filename = connection.recv(filename_len).decode()
-
-    if command == b'UPLOAD':
-        handle_upload(connection, filename)
-    elif command == b'DOWNLOAD':
-        handle_download(connection, filename)
-
-def handle_upload(connection, filename):
-    data_length = int(connection.recv(16).decode())
-    data = connection.recv(data_length)
-    while len(data) < data_length:
-        data += connection.recv(4096)
-        
-    nonce, ciphertext, tag, file_hash = data[:16], data[16:-48], data[-48:-32], data[-32:].decode()
-    
-    file_path = os.path.join(UPLOAD_DIR, filename)
-    
-    with open(file_path, 'wb') as f:
-        f.write(nonce + ciphertext + tag)
-    
-    if sha256_hash(file_path) != file_hash:
-        connection.sendall(b'File integrity check failed')
-    else:
-        connection.sendall(b'File received successfully')
-
-def handle_download(connection, filename):
-    file_path = os.path.join(UPLOAD_DIR, filename)
-    if not os.path.exists(file_path):
-        connection.sendall(b'File not found')
-        return
-    
-    with open(file_path, 'rb') as f:
-        data = f.read()
-    
-    nonce, ciphertext, tag = data[:16], data[16:-32], data[-32:]
-    file_hash = sha256_hash(file_path)
-    
-    data = nonce + ciphertext + tag + file_hash.encode()
-    connection.sendall(f'{len(data):016d}'.encode())  # 发送数据长度
-    connection.sendall(data)
-
-
-def run_server():
-    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    context.load_cert_chain(certfile="certs/server.crt", keyfile="certs/server.key")
-    # 确保上传目录存在
-    os.makedirs(UPLOAD_DIR, exist_ok=True)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-        sock.bind((SERVER_ADDRESS, SERVER_PORT))
-        sock.listen(5)
-        print("Server Listening...")
-        with context.wrap_socket(sock, server_side=True) as ssock:
-            while True:
-                connection, client_address = ssock.accept()
-                handle_client(connection)
-                connection.close()
 
 
 class server:
@@ -123,7 +90,6 @@ class server:
             fileinfo_size = struct.calcsize('128sl')
             # 接收文件名与文件大小信息
             buf = conn.recv(fileinfo_size)
-            print(len(buf))
             # 判断是否接收到文件头信息
             if buf:
                 header_json = str(struct.unpack('128s', buf)[0], encoding='utf-8').strip('\00')
@@ -143,7 +109,7 @@ class server:
         # 定义接收了的文件大小
         recvd_size = 0
         # 存储在uploaded_files目录中
-        fp = open("/uploaded_files" + str(file_name), "wb")
+        fp = open("uploaded_files/" + str(file_name), "wb")
         print("Start receiving")  
         while not recvd_size == file_size:
             if file_size - recvd_size > 1024:
@@ -157,45 +123,47 @@ class server:
                 len = int(conn.recv(1024).decode("utf-8"))
                 print("该段发送长度: ", len)
                 rdata = conn.recv(len)
+                # print(rdata)
                 decrypted_data = decrypt_file(rdata)
                 recvd_size = file_size
-            fp.write(rdata)
+            fp.write(decrypted_data)
         fp.close()
         print('receive done')
         conn.close()
 
     def handle_download(self, conn, header):
+        #TODO
         pass
     
+
 def decrypt_file(data):
     rsa = RSACryptor()
     cipher_message, cipher_keyiv = pickle.loads(data)
-    print("密文:{}(类型{}) \n 密钥:{}(类型{})".format(cipher_message, type(cipher_message), cipher_keyiv, type(cipher_keyiv)))
-    # decrypted_keyiv = rsa.decrypt_message(cipher_keyiv, SERVER_PRIVATE_KEY)
+    print("密文:{}(类型{})\n密钥:{}(类型{})".format(cipher_message, type(cipher_message), cipher_keyiv, type(cipher_keyiv)))
+    decrypted_keyiv = rsa.decrypt_message(cipher_keyiv, SERVER_PRIVATE_KEY)
     # print("接收到的密钥和初始向量:", decrypted_keyiv)
-    # keyiv = pickle.loads(decrypted_keyiv)
-    # key, iv = keyiv["Key"], keyiv["IV"]
-    # aes = AESCryptor(key, iv)
-    # decrypted_message = aes.decrypt_message(cipher_message)
-    # plain_message = pickle.loads(decrypted_message)
-    # content = base64.b64decode(plain_message['Message'])
-    # print('传送的内容是', content)
-    # digest = plain_message['digest']
-    # if RSACryptor.verify_signature(content, digest, CLIENT_PUBLIC_KEY):
-    #     return content
-    # else:
-    #     print("文件签名不一致!")
+    keyiv = pickle.loads(decrypted_keyiv)
+    key, iv = keyiv["Key"], keyiv["IV"]
+    print("解密后的密钥{}和初始向量{}:".format(key, iv))
+    aes = AESCryptor(key, iv)
+    decrypted_message = aes.decrypt_message(cipher_message)
+    plain_message = pickle.loads(decrypted_message)
+    content = base64.b64decode(plain_message['Message'])
+    print("解密的内容是", content)
+    digest = plain_message['Digest']
+    print("解密的消息摘要", digest, type(digest))
+    if rsa.verify_signature(content, digest, CLIENT_PUBLIC_KEY):
+        print("完整性验证通过!")
+        return content
+    else:
+        print("文件签名不一致!")
 
 
 if __name__ == "__main__":
-    # server = server()
-    # server.listen()
-    # 发送的内容: b'1222222'
-    # aes加密密钥: b'JLlTu52jAHXEmw7K' aes初始向量: b"\xc7{'\x0f\xc2}\x1b\xbcV\xac\xb5\xd1\xf5g,\xe6"
-    # 加密后的消息: b'\x80\x04\x95A\x04\x00\x00\x00\x00\x00\x00]\x94(X\xd8\x02\x00\x002jDP4bXbVnBMp29wn0SmfoPCrBvyg7WO81VArJ9SGJR6dBmzDrTDc5dR4BKDpW59vMR+tonpWox/nU+5/H9Gk269CG+Xe2+giQ1m4yZ1QGc29c3lXkqvR8C0H5o8UiZhqBLJE7A7XjLPdvwJLwRyNYq7E2PbPTfd6YUedV6icLxWLM7Nmh9xGSqBYLZCl5t2rRx3trwnczopxIoh/vOyHGgcBUMr99LLS/Tj6fcjwUEMBbziquwc2sg77a6JNom31KKbzMBhUH4TLxAnN/V4qJ1PDGRj4UMdeMHu1A0I7wF3WmqZVVOPTltIWfeAj+wm9OVOexT9idkW7RrnQ4RgQt0zoecIapsYA/Db2smtR3BOR0eTG3LhCSnYEaJR5lOlVZw5LgsOOQnP0QJXK4VirfmaI/TqRBhmSUp9b7+RG89xKIYlAyPYdcZlbGtsvSW4Em63SYR/VCHBFRRudlN9yAmRwQwQLecOfmBsFB3ecP/WMhaHxk1mLxnzpS+iNAqEhUQQTxKncCJ+DDYvTeyx9TfkMolT3Juudt+PQQ8XrxjVnqmSSpyXU/ctAJ7N5voCQuw7iFISxmsVBVgcv1CvOIEtwU0CmXpwKFRFU5l59BnjBBtW5V89e2i06UJxFPc30kj0vDZ1LOwBl4jE3MQTfmg2mh1mBGvd+X2VZSxoYrOT3bsEMiYWEuA4FwBugn9mCO4/CzDtIQXsmv1eU0GtLQ==\x94BX\x01\x00\x00suHEiAXZFwBG2EniQcZO8YgRZqagWWN5G4PUz/1Rl1VCeO8fc6QxmKXSebsjKXvoE+QNSZPPzY5zAYsTAvBLabaO4qZnGfNS6FY2lbnBVNnue8buV4x/Z7eNftZ9TisGJanJXPWdm2u07bJwRIoEn41dTPQ+Mlny+5gGUBmaJcsPvOdqZufNd/ZTKtRK2ovRtxtWFyKNkvdH8F9AY5Y+Fp8X7ptUnbFbF11UbFb8bySl8CT4TZxl1u/iGxOKIbQj/2Ev91egW7bNi4/pwL3bSH5CZHYKK5Tm9NaAULoIrDXaPSFriSyBvvRjvvHraM7E/sKeXZUOyq7YMdhQK0GF0w==\x94e.'
-    init_key()
-    data = b'\x80\x04\x95A\x04\x00\x00\x00\x00\x00\x00]\x94(X\xd8\x02\x00\x002jDP4bXbVnBMp29wn0SmfoPCrBvyg7WO81VArJ9SGJR6dBmzDrTDc5dR4BKDpW59vMR+tonpWox/nU+5/H9Gk269CG+Xe2+giQ1m4yZ1QGc29c3lXkqvR8C0H5o8UiZhqBLJE7A7XjLPdvwJLwRyNYq7E2PbPTfd6YUedV6icLxWLM7Nmh9xGSqBYLZCl5t2rRx3trwnczopxIoh/vOyHGgcBUMr99LLS/Tj6fcjwUEMBbziquwc2sg77a6JNom31KKbzMBhUH4TLxAnN/V4qJ1PDGRj4UMdeMHu1A0I7wF3WmqZVVOPTltIWfeAj+wm9OVOexT9idkW7RrnQ4RgQt0zoecIapsYA/Db2smtR3BOR0eTG3LhCSnYEaJR5lOlVZw5LgsOOQnP0QJXK4VirfmaI/TqRBhmSUp9b7+RG89xKIYlAyPYdcZlbGtsvSW4Em63SYR/VCHBFRRudlN9yAmRwQwQLecOfmBsFB3ecP/WMhaHxk1mLxnzpS+iNAqEhUQQTxKncCJ+DDYvTeyx9TfkMolT3Juudt+PQQ8XrxjVnqmSSpyXU/ctAJ7N5voCQuw7iFISxmsVBVgcv1CvOIEtwU0CmXpwKFRFU5l59BnjBBtW5V89e2i06UJxFPc30kj0vDZ1LOwBl4jE3MQTfmg2mh1mBGvd+X2VZSxoYrOT3bsEMiYWEuA4FwBugn9mCO4/CzDtIQXsmv1eU0GtLQ==\x94BX\x01\x00\x00suHEiAXZFwBG2EniQcZO8YgRZqagWWN5G4PUz/1Rl1VCeO8fc6QxmKXSebsjKXvoE+QNSZPPzY5zAYsTAvBLabaO4qZnGfNS6FY2lbnBVNnue8buV4x/Z7eNftZ9TisGJanJXPWdm2u07bJwRIoEn41dTPQ+Mlny+5gGUBmaJcsPvOdqZufNd/ZTKtRK2ovRtxtWFyKNkvdH8F9AY5Y+Fp8X7ptUnbFbF11UbFb8bySl8CT4TZxl1u/iGxOKIbQj/2Ev91egW7bNi4/pwL3bSH5CZHYKK5Tm9NaAULoIrDXaPSFriSyBvvRjvvHraM7E/sKeXZUOyq7YMdhQK0GF0w==\x94e.'
-    decrypt_file(data)
+    # init_key()
+    server = server()
+    server.listen()
+    
 
 
 

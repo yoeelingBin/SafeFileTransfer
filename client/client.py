85b6b2fa3199d0b7dca5669fdfdf48ed0199f608
Yan Qibin <57822313+yoeelingBin@users.noreply.github.com>
2024-06-14 21:52:30 +0800

    reconstruct client class

--- client/client.py
+++ client/client.py
@@ -8,19 +8,8 @@ import time
 import json
 from common.AESencryption import AESCryptor
 from common.RSAencryption import RSACryptor
+from common.utils import sha256_hash
 
-SERVER_ADDRESS = '127.0.0.1'
-SERVER_PORT = 12345
-SERVER_PUBLIC_KEY = '''-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv8TQORU+5RHbuXv40mtF
-CYCmG3ML2a4VkhtLEDRArO0M+DpbBlikbw5hPVHwcfSyqayFRFA+VUzSa8jwTwUL
-z2MgCSav57nouwpGqJA8hznezTplXMU1Gbjic8N1MDwIaRAtnQrgS+bRrrp/1ry+
-ygNcJRlt8e0hfJR1arHVqSKW897GcnGYXwVKZ7AwUHW54PzPegfc8lMGVWPHctdr
-iYcMCynUK8LDGraayg06vy6OwsLI15m2W+q2eUbrdcHFYV3hmeWNhhvcgMPdYnfW
-aG//ImhD/rO4zLNZjtrKzsYWeGpvrmwgg5czU7X3rl2fm9Shhm9iB8c3QmF+tuXM
-OQIDAQAB
------END PUBLIC KEY-----
-'''
 CLIENT_PRIVATE_KEY = '''-----BEGIN RSA PRIVATE KEY-----
 MIIEowIBAAKCAQEA3KVnjnA2BXxf6leV3EjRSdAHTdEHwmjjiJgFFBJiB+V4RCAE
 FDWiRA8tiU+LkAPZwH/XE8dWkO/cCc3f6LHrMC/BosVPLvxMWrkkQkH9eS/eTja9
@@ -94,52 +83,177 @@ class Client:
 
     Attributes: None
     '''
+    SERVER_ADDRESS = '127.0.0.1'
+    SERVER_PORT = 12345
+    
     def __init__(self) -> None:
+        self.ssock = self._initialize_ssl_connection()
+        self.rsa_cipher = RSACryptor()
+        self.aes_key = AESCryptor.gen_key()
+        self.aes_iv = AESCryptor.gen_iv()
+        self.aes_cipher = AESCryptor(self.aes_key, iv=self.aes_iv)
+        self.server_public_key = '''-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv8TQORU+5RHbuXv40mtF
+CYCmG3ML2a4VkhtLEDRArO0M+DpbBlikbw5hPVHwcfSyqayFRFA+VUzSa8jwTwUL
+z2MgCSav57nouwpGqJA8hznezTplXMU1Gbjic8N1MDwIaRAtnQrgS+bRrrp/1ry+
+ygNcJRlt8e0hfJR1arHVqSKW897GcnGYXwVKZ7AwUHW54PzPegfc8lMGVWPHctdr
+iYcMCynUK8LDGraayg06vy6OwsLI15m2W+q2eUbrdcHFYV3hmeWNhhvcgMPdYnfW
+aG//ImhD/rO4zLNZjtrKzsYWeGpvrmwgg5czU7X3rl2fm9Shhm9iB8c3QmF+tuXM
+OQIDAQAB
+-----END PUBLIC KEY-----
+'''
+
+    def _initialize_ssl_connection(self):
         context = ssl.create_default_context()
-        # 信任自签名证书
-        context.load_verify_locations("certs/server.crt") 
-        # 连接服务端
-        self.sock = socket.create_connection((SERVER_ADDRESS, SERVER_PORT))
-        # 将socket打包成SSL socket
-        self.ssock = context.wrap_socket(self.sock, server_hostname=SERVER_ADDRESS)
+        context.load_verify_locations("certs/server.crt")
+        sock = socket.create_connection((self.SERVER_ADDRESS, self.SERVER_PORT))
+        return context.wrap_socket(sock, server_hostname=self.SERVER_ADDRESS)
+    
+    def connect(self):
+        '''
+        Usage: 与服务端进行连接
+        '''
+        print("开始交换公钥")
+        self.exchange_pubkey("keys/client/clientpublic.pem")
+        self.verify_key(self.ssock)
+
+    def exchange_pubkey(self, key_dir: str):
+        '''
+        Usage: 向服务端发送客户端公钥
+
+        Args:
+            key_dir: 存放公钥的目录
+        '''
+        with open(key_dir, 'rb') as fi:
+            public_key = fi.read()
+        key_hash = sha256_hash(public_key)
+        hash_message = pickle.dumps([public_key, key_hash])
+        for _ in range(3):  # 尝试三次
+            try:
+                self.ssock.send(hash_message)
+                print("公钥发送成功")
+                return
+            except ConnectionRefusedError:
+                print("公钥发送失败，尝试重新发送")
+        print("公钥发送失败")
+    
+    def verify_key(self, sock):
+        '''
+        Usage: 对服务端公钥完整性进行验证
+
+        Args:
+            sock: SSLSocket
+        '''
+        while True:
+            message = sock.recv(4096)
+            (public_key, key_hash) = pickle.loads(message)
+            if key_hash == sha256_hash(public_key):
+                print("公钥完整性验证完成，可以开始传输文件\n")
+                self.server_public_key = public_key
+                print("收到公钥\n" +  self.server_public_key.decode('utf-8') + "\n")
+                break
     
     def upload_file(self, file_path: str):
         '''
-        Usage: 客户端上传文件
+        客户端上传文件
         
         Args: 
             file_path: 文件路径
         '''
-        if os.path.isfile(file_path):
-            # 定义打包规则
-            # fileinfo_size = struct.calcsize('128sl')
-            # 定义文件头信息，包含文件名和文件大小
-            header = {
-                'command': 'UPLOAD',
-                'fileName': os.path.basename(file_path),
-                'fileSize': os.stat(file_path).st_size,
-                'time': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
-            }
-            header_bytes = bytes(json.dumps(header).encode("utf-8"))
-            fhead = struct.pack('128s', header_bytes)
-            # 发送文件名称与文件大小
-            print(fhead)
-            self.ssock.send(fhead)
-            # 将传输文件以二进制的形式分多次上传至服务器
-            with open(file_path, 'rb') as fp:
-                while True:
-                    data = fp.read(1024)
-                    if not data:
-                        print(f'{os.path.basename(file_path)}文件发送完毕...')
-                        break
-                    print("发送的内容:", data)
-                    tosend = encrypt_file(data)
-                    print("加密后的消息:", tosend)
-                    self.ssock.send(str(len(tosend)).encode('utf-8'))
-                    self.ssock.send(tosend)
+        try:
+            if os.path.isfile(file_path):
+                self._send_file_header(file_path)
+                self._send_file_content(file_path)
+            else:
+                print(f"文件 {file_path} 不存在")
+        except Exception as e:
+            print(f"上传文件时发生错误: {e}")
+            
 
-            # fp.close()
-            self.ssock.close()
+    def _send_file_header(self, file_path: str):
+        header = {
+            'command': 'UPLOAD',
+            'fileName': os.path.basename(file_path),
+            'fileSize': os.stat(file_path).st_size,
+            'time': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+        }
+        header_bytes = bytes(json.dumps(header).encode("utf-8"))
+        fhead = struct.pack('128s', header_bytes)
+        self.ssock.send(fhead)
+
+    def _send_file_content(self, file_path: str):
+        with open(file_path, 'rb') as fp:
+            while True:
+                data = fp.read(1024)
+                if not data:
+                    print(f'{os.path.basename(file_path)}文件发送完毕...')
+                    break
+                print("发送的内容:", data)
+                tosend = self.encrypt_file(data)
+                print("加密后的消息", tosend)
+                self.ssock.send(str(len(tosend)).encode('utf-8'))
+                self.ssock.send(tosend)
+
+    def encrypt_file(self, data) -> bytes:
+        '''
+        加密二进制数据
+        
+        Args: 
+            data: 需要加密数据
+        Returns:
+            加密后的数据
+        '''
+        print("aes加密密钥:", self.aes_key, "aes初始向量:", self.aes_iv)
+        
+        digest = self.rsa_cipher.sign_message(data, CLIENT_PRIVATE_KEY)
+        print("消息摘要:", digest)
+        
+        concated_message = {"Message": base64.b64encode(data), "Digest": digest}
+        dumpped_message = pickle.dumps(concated_message)
+        
+        cipher_message = self.aes_cipher.encrypt_message(dumpped_message)
+        
+        keyiv = {"Key": self.aes_key, "IV": self.aes_iv}
+        print("密钥和初始向量", keyiv)
+        dumpped_keyiv = pickle.dumps(keyiv)
+        print("序列化后的密钥和初始向量", dumpped_keyiv)
+        
+        cipher_keyiv = self.rsa_cipher.encrypt_message(dumpped_keyiv, self.server_public_key)
+        print("加密后的密钥和初始向量", cipher_keyiv)
+        
+        send_message = pickle.dumps([cipher_message, cipher_keyiv])
+        print("序列化前的发送消息", [cipher_message, cipher_keyiv])
+        return send_message
+
+    def decrypt_file(self, data) -> bytes:
+        '''
+        解密二进制数据
+        
+        Args: 
+            data: 需要解密的数据
+        Returns:
+            解密后的数据(完整性通过),否则返回None
+        '''
+        cipher_message, cipher_keyiv = pickle.loads(data)
+        print(f"密文:{cipher_message}, 类型{type(cipher_message)}\n密钥:{cipher_keyiv}, 类型{type(cipher_keyiv)}")
+        decrypted_keyiv = self.rsa_cipher.decrypt_message(cipher_keyiv, SERVER_PRIVATE_KEY)
+        keyiv = pickle.loads(decrypted_keyiv)
+        key, iv = keyiv["Key"], keyiv["IV"]
+        print(f"解密后的密钥{key}和初始向量{iv}:")
+        
+        aes = AESCryptor(key, iv)
+        decrypted_message = aes.decrypt_message(cipher_message)
+        plain_message = pickle.loads(decrypted_message)
+        content = base64.b64decode(plain_message['Message'])
+        print("解密的内容是", content)
+        digest = plain_message['Digest']
+        print("解密的消息摘要", digest, type(digest))
+        
+        if self.rsa_cipher.verify_signature(content, digest, CLIENT_PUBLIC_KEY):
+            return content
+        else:
+            print("文件签名不一致!")
+            return None
     
     def download_file(self, file_path: str):
         '''
@@ -151,102 +265,13 @@ class Client:
         # TODO: 
 
 
-def encrypt_file(data) -> bytes:
-    '''
-    Usage: 加密二进制数据
-        
-    Args: 
-        data: 需要加密数据
-    Returns:
-        加密后的数据
-    '''
-    # RSA初始化
-    rsa_cipher = RSACryptor()
-    # AES初始化
-    aes_key = AESCryptor.gen_key()
-    aes_iv = AESCryptor.gen_iv()
-    aes_cipher = AESCryptor(aes_key, iv=aes_iv)
-    print("aes加密密钥:", aes_key, "aes初始向量:", aes_iv)
-    # 消息摘要
-    digest = rsa_cipher.sign_message(data, CLIENT_PRIVATE_KEY)
-    print("消息摘要:", digest)
-    # 将摘要和消息打包
-    concated_message = {"Message": base64.b64encode(data), "Digest": digest}
-    dumpped_message = pickle.dumps(concated_message)
-    # 对称加密消息
-    cipher_message = aes_cipher.encrypt_message(dumpped_message)
-    # 密钥和初始向量
-    keyiv = {"Key": aes_key, "IV": aes_iv}
-    print("密钥和初始向量", keyiv)
-    dumpped_keyiv = pickle.dumps(keyiv)
-    print("序列化后的密钥和初始向量", dumpped_keyiv)
-    # 非对称加密密钥
-    cipher_keyiv = rsa_cipher.encrypt_message(dumpped_keyiv, SERVER_PUBLIC_KEY)
-    print("加密后的密钥和初始向量", cipher_keyiv)
-    # 发送消息
-    print("序列化前的发送消息", [cipher_message, cipher_keyiv])
-    send_message = pickle.dumps([cipher_message, cipher_keyiv])
-    # print("发送的消息:", send_message)
-    return send_message
-
-def decrypt_file(data) -> bytes:
-    '''
-    Usage: 解密二进制数据
-        
-    Args: 
-        data: 需要解密的数据
-    Returns:
-        解密后的数据(完整性通过),否则返回None
-    '''
-    rsa = RSACryptor()
-    cipher_message, cipher_keyiv = pickle.loads(data)
-    print(f"密文:{cipher_message}, 类型{type(cipher_message)}\n密钥:{cipher_keyiv}, 类型{type(cipher_keyiv)}")
-    decrypted_keyiv = rsa.decrypt_message(cipher_keyiv, SERVER_PRIVATE_KEY)
-    # print("接收到的密钥和初始向量:", decrypted_keyiv)
-    keyiv = pickle.loads(decrypted_keyiv)
-    key, iv = keyiv["Key"], keyiv["IV"]
-    print(f"解密后的密钥{key}和初始向量{iv}:")
-    aes = AESCryptor(key, iv)
-    decrypted_message = aes.decrypt_message(cipher_message)
-    plain_message = pickle.loads(decrypted_message)
-    content = base64.b64decode(plain_message['Message'])
-    print("解密的内容是", content)
-    digest = plain_message['Digest']
-    print("解密的消息摘要", digest, type(digest))
-    if rsa.verify_signature(content, digest, CLIENT_PUBLIC_KEY):
-        return content
-    else:
-        print("文件签名不一致!")
-        return None
-
-# def upload_file(file_path):
-#     nonce, ciphertext, tag = encrypt_file(file_path)
-#     file_hash = sha256_hash(file_path)
-#     filename = file_path.split('\\')[-1]
-#     filename_bytes = filename.encode()
-    
-#     context = ssl.create_default_context()
-#     context.load_verify_locations("certs/server.crt") # 信任自签名证书
-#     with socket.create_connection((SERVER_ADDRESS, SERVER_PORT)) as sock:
-#         with context.wrap_socket(sock, server_hostname=SERVER_ADDRESS) as ssock:
-#             header = b'UPLOAD' + f'{len(filename_bytes):016d}'.encode()
-#             ssock.sendall(header)
-#             ssock.sendall(filename_bytes)
-#             data = nonce + ciphertext + tag + file_hash.encode()
-#             ssock.sendall(f'{len(data):016d}'.encode())  # 发送数据长度
-#             ssock.sendall(data)
-#             response = ssock.recv(4096)
-#             print(response.decode())
-
-#     print("File send success")
-
 if __name__ == "__main__":
     mes = b"kskskkqqqhfhfh1234"
     file1_path = "test_files/1.txt"
     file2_path = "test_files/2.md"
     file3_path = "test_files/3.jpg"
     client = Client()
-    client.upload_file(file3_path)
+    client.upload_file(file2_path)
     # fp = open(file_path, 'rb')
     # data = fp.read(1024)
     # enc_data = encrypt_file(data)
--- common/utils.py
+++ common/utils.py
@@ -1,8 +1,20 @@
 import hashlib
 
-def sha256_hash(file_path):
+def sha256_hash(data: str):
+    '''
+    Usage: 进行sha256哈希
+    
+    Args:
+        data: 需要哈希的字符串
+    Returns:
+        进行哈希和消息摘要后的字符串
+    '''
     sha256 = hashlib.sha256()
-    with open(file_path, 'rb') as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            sha256.update(chunk)
-    return sha256.hexdigest()
+    sha256.update(data)
+    res = sha256.hexdigest()
+    return res
+
+if __name__ == '__main__':
+    e = sha256_hash('你好'.encode('utf-8'))
+    print(e)
+    
